8f171e5e50869d58ef1be2093558055541cca04b
robertu <4065233+robertu7@users.noreply.github.com>
2024-01-26 18:30:25 +0800

    feat: simulate transaction before sending it

--- src/pages/api/clear-auctions/[...params].ts
+++ src/pages/api/clear-auctions/[...params].ts
@@ -1,5 +1,11 @@
-import { clearAuctions } from "@/utils";
+import {
+  billboardContract,
+  getClearableAuctions,
+  publicClient,
+  walletClient,
+} from "@/utils";
 import { NextApiRequest, NextApiResponse } from "next";
+import { SimulateContractErrorType } from "viem";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // if (
@@ -18,7 +24,54 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   try {
-    await clearAuctions({ from, to });
+    // get auctions to be cleared
+    const auctions = await getClearableAuctions({ from, to });
+
+    if (!auctions || auctions.length <= 0) {
+      return;
+    }
+
+    // Step 1: Get total tax of next auction
+    let clearAuctionRequest: any;
+    let totalTax: BigInt;
+    try {
+      const result = await publicClient.simulateContract({
+        ...billboardContract,
+        functionName: "clearAuctions",
+        args: [auctions.map(({ tokenId }) => tokenId)],
+      });
+      clearAuctionRequest = result.request;
+      totalTax = result.result[1].reduce((acc, cur) => acc + cur, BigInt(0));
+    } catch (err) {
+      const error = err as SimulateContractErrorType;
+      console.error(error.name, err);
+
+      // if (err instanceof BaseError) {
+      //   const revertError = err.walk(
+      //     (err) => err instanceof ContractFunctionRevertedError,
+      //   );
+      //   if (revertError instanceof ContractFunctionRevertedError) {
+      //     const errorName = revertError.data?.errorName ?? "";
+      //     console.error(errorName, err);
+      //   }
+      // }
+      throw err;
+    }
+
+    // Step 2: Get merkle root
+
+    // Step 3: Multicall to
+    //   1. clear auction
+    //   2. withdraw tax
+    //   2. create new drop with merkle root and tax
+    await publicClient.multicall({
+      contracts: [
+        clearAuctionRequest,
+        // 2
+        // 3
+      ],
+    });
+    // await walletClient.writeContract(request);
 
     return res.status(200).json({ success: true, now: Date.now() });
   } catch (error) {
